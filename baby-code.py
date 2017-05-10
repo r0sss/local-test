6c4020c3c9978fac3cbf2e9da3057aa98239b05a
DESKTOP-26RA7NE\ross <ross.ladley@gmail.com>
2017-05-10 17:03:20 -0400

    More lyric text, more config-- this time local.

--- baby-code.py
+++ baby-code.py
@@ -4,7 +4,7 @@ x='Another one!'
 y='-DJ Khaled'
 z='All I do is win, win, win-- no matter what'
 a='got money on my mind, I can never get enough'
-b='and everytime I step into the building, everybody hands go up'
+b='and everytime I step into the building, everybody hands go up....... and they stay there'
 
 print(x)
 print(z)
