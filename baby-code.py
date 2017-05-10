8f61392143f90eb916e440df551549864503756b
DESKTOP-26RA7NE\ross <ross.ladley@gmail.com>
2017-05-10 17:23:09 -0400

    Clearing up the 'sasquatch'-related confusion

--- baby-code.py
+++ baby-code.py
@@ -4,18 +4,16 @@ x='Another one!'
 y='-DJ Khaled'
 z='All I do is win, win, win-- no matter what'
 a='got money on my mind, I can never get enough'
-<<<<<<< HEAD
-b='and everytime I step into the building, everybody hands go up....... and they stay there'
-c='--Michael Scott'
-=======
 b='and everytime I step into the building, everybody hands go up....... and they actually end up going down afterwhile.'
->>>>>>> develop
+c='--Michael Scott'
+
 
 print(x)
 print(z)
 print(a)
 print(b)
 print(y)
+print(c)
 
 
 
