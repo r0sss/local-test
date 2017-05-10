b9ef58ea8a68fdb34e55a9b719269d27cc3d780c
DESKTOP-26RA7NE\ross <ross.ladley@gmail.com>
2017-05-10 17:07:27 -0400

    Further iteration's of messin' with sasquatch. With two branches.

--- baby-code.py
+++ baby-code.py
@@ -4,7 +4,7 @@ x='Another one!'
 y='-DJ Khaled'
 z='All I do is win, win, win-- no matter what'
 a='got money on my mind, I can never get enough'
-b='and everytime I step into the building, everybody hands go up....... and they stay there'
+b='and everytime I step into the building, everybody hands go up....... and they actually end up going down afterwhile.'
 
 print(x)
 print(z)
