d1b1c3dd453886a85f70f4840867f71f19d8dc10
DESKTOP-26RA7NE\ross <ross.ladley@gmail.com>
2017-05-10 16:51:22 -0400

    Add a new config and new lyric text.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # local-test
 Creating repo to work solely on SourceTree and local editors.
 Adding another line to see additional changes-- but for real this time.
+
+Config file change1
--- baby-code.py
+++ baby-code.py
@@ -2,9 +2,16 @@
 
 x='Another one!'
 y='-DJ Khaled'
-z='All I do is win'
+z='All I do is win, win, win-- no matter what'
+a='got money on my mind, I can never get enough'
+b='and everytime I step into the building, everybody hands go up'
 
 print(x)
 print(z)
+print(a)
+print(b)
 print(y)
+
+
+
 
